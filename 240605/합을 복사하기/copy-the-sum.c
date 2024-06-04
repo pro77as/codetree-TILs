ae99020f268b8041184662f57c584d7aa906d8b0
pro77as <117465635+pro77as@users.noreply.github.com>
2024-06-05 02:34:14 +0900

    Title: 합을 복사하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240605/합을 복사하기/copy-the-sum.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a = 1;
+    int b = 2;
+    int c = 3;
+    int temp;
+    temp = a+b+c;
+    a = b = c = temp;
+    printf("%d %d %d",a,b,c);
+    return 0;
+}
